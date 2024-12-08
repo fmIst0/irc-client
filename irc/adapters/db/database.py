aaabbfa41d838ea0df200a8dee0175f84a92b498
kharchenko_bohdan <bohdan.kharchenko@l-center.com>
2024-12-08 15:54:55 +0200

    lab4 singleton

--- irc/adapters/db/database.py
+++ irc/adapters/db/database.py
@@ -10,10 +10,22 @@ DATABASE_URL = (
     "postgresql+asyncpg://user:password@db_platform:5432/db_platform"
 )
 
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
-)
+
+class Database:
+    _instance = None
+
+    def __new__(cls, database_url: str):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.engine = create_async_engine(database_url)
+            cls._instance.session_maker = async_sessionmaker(
+                cls._instance.engine, expire_on_commit=False
+            )
+        return cls._instance
+
+
+db = Database(DATABASE_URL)
+async_session_maker = db.session_maker
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
